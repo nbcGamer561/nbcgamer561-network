c24385e58ca785c50955136fc7a4fdc4c40fc88b
Marvin <48691487+nbcGamer561@users.noreply.github.com>
2019-05-09 17:36:49 -0400

    Create privacy-tools.php

---
+++ php/privacy-tools.php
@@ -0,0 +1,83 @@
+<link rel="shortcut icon" type="image.png" href="favicon.png">
+<title>nbcGamer561</title>
+<div id="container">
+<div id="header">
+<h1>nbcGamer561</h1>
+</div>
+<div id="content">
+<div id="nav">
+<h3>Navigation</h3>
+<ul>
+<li><a href="index.html">Home</li></a>
+<li><a href="server.html">Servers<li></a>
+    <li><a href="about.html">About</a></li>
+    <li><a class="selected" href="">Privacy Tools<li></a>
+</ul>
+</div>
+<div id="main">
+<h2>Privacy Tools</h2>
+<li><a href="ip.php">What's My IP Address</a></li>
+</div>
+</div>
+<div id="footer">
+<p>Copyright &copy; 2019 nbcGamer561</p>
+<style>
+#header {
+background-color: #2B65EC;
+color: white;
+text-align: center;
+padding: 10px
+}
+body {
+background-color: #E5E4E2;
+font-family: Helvetica, Arial, san-serif;
+
+}
+#container {
+background-color: white;
+width: 800px;
+margin-left: auto;
+margin-right: auto;
+
+}
+#content {
+padding: 10px;
+
+}
+
+
+#nav {
+width: 180px;
+float: left;
+
+
+}
+#main {
+width: 600px;
+float: right;
+
+}
+
+#footer {
+clear: both;
+padding: 10px;
+background-color: #2B65EC;
+color: white;
+text-align: right;
+
+}
+
+#nav .selected {
+font-weight: bold;
+}
+
+
+a {
+text-decoration: none;
+color: blue;
+}
+
+#nav ul {
+list-style-type: none;
+padding: 0px;
+</style>
